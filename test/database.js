58accce6eb781baa8b3fd86a6acc233adac1e971
brainpoint <bpoint.lee@gmail.com>
2017-06-06 17:05:09 +0800

    [doc] 配置调整

--- test/database.js
+++ test/database.js
@@ -19,30 +19,17 @@ var co        = require('co');
 module.exports = class Test extends database {
   constructor() {
     // create.
-
-    // super('mysql', {
-    //   queryTimeout      : 5000,
-    //   connectTimeout    : 5000,
-    //   acquireTimeout    : 5000,
-    //   queueLimit        : 20,
-    //   connectionLimit   : 5,
-    //   host              : '112.124.106.204',
-    //   port              : 3306,
-    //   user              : 'test',
-    //   password          : 'test',
-    //   database          : 'test',
-    // });
     super('mssql', {
       queryTimeout      : 5000,
       connectTimeout    : 5000,
       acquireTimeout    : 5000,
       queueLimit        : 20,
       connectionLimit   : 5,
-      host              : '106.14.1.123',
-      port              : 14434,
-      user              : 'tester',
-      password          : 'My@Super@Secret',
-      database          : 'testdb',
+      host              : '',
+      port              : 1443,
+      user              : '',
+      password          : '',
+      database          : '',
     });
 
     // table.
